00e47dff30797520f97d6c65153e98d2b6d49bba
Dusan Radojcic <dukir00@gmail.com>
2020-04-04 01:45:12 +0200

    Pocetak izrade logike calculatora

--- calculator/script.js
+++ calculator/script.js
@@ -1,15 +1,60 @@
-function sum(){
-
+function sum(a, b) {
+    return a + b;
 }
 
-function minus(){
-
+function minus(a, b) {
+    return a - b;
 }
 
-function multiply(){
-
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(){
-    
+function divide(a, b) {
+    return b === 0 ? NaN : a / b;
+}
+
+const buttons = document.querySelectorAll('#buttons button');
+const display = document.querySelector('#display p');
+
+console.table(buttons);
+
+buttons.forEach(button => button.addEventListener('click', displayNumbers));
+
+var firstNumber = 0;
+var currentNumber = 0;
+function displayNumbers() {
+
+    switch (this.value) {
+        case '=':
+            console.log("Jednako");
+            break;
+        case 'c':
+            console.log('clear');
+            break;
+        case 'ce':
+            console.log('clear');
+            break;
+        case 'del':
+            console.log('del');
+            break;
+        case '-':
+            console.log('minus');
+            break;
+        case '+':
+            firstNumber = currentNumber;
+            currentNumber = 0;
+            break
+        case '*':
+            firstNumber = currentNumber;
+            currentNumber = 0;
+            break;
+        case '/':
+            firstNumber = currentNumber;
+            currentNumber = 0;
+            break;
+        default:
+            currentNumber = currentNumber * 10 + parseInt(this.value);
+            display.textContent = currentNumber;
+    }
 }
